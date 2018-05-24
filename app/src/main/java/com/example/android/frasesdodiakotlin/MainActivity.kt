8974ba5205deaae2309a9cedd3a2fb5818b0d364
Ytallo de Lima Gadelha <ytallolg@gmail.com>
2018-05-24 20:00:37 -0300

    Adição da lógica da aplicação

--- app/src/main/java/com/example/android/frasesdodiakotlin/MainActivity.kt
+++ app/src/main/java/com/example/android/frasesdodiakotlin/MainActivity.kt
@@ -2,11 +2,33 @@ package com.example.android.frasesdodiakotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var texto: TextView
+    val frases = arrayOf(
+            "O importante não é vencer toddos os dias, mas lutar sempre!",
+            "Maior que a tristeza de não haver vencido é a vergonha de não ter lutado!",
+            "É melhor conquistar a si mesmo do que vencer mil batalhas!",
+            "Enquanto houver vontade de lutar haverá esperança de vencer!")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        texto = findViewById(R.id.textoFrase) as TextView
+
+    }
+
+    fun gerarFrase(view: View){
+
+        val totalItensArray = frases.size
+        val numeroAleatorio = Random().nextInt(totalItensArray)
+
+        texto.setText(frases[numeroAleatorio])
+
     }
 }
